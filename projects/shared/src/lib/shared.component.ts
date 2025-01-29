d6cc4b27cd423fd32abf42c6b5e4deb95da7961d
msaadart <saadnasir99@gmail.com>
2025-01-30 00:18:41 +0500

    fix: Fix a bug

--- projects/shared/src/lib/shared.component.ts
+++ projects/shared/src/lib/shared.component.ts
@@ -14,3 +14,37 @@ import { Component } from '@angular/core';
 export class SharedComponent {
 
 }
+
+
+// name: Publish Angular Library to GitHub Packages
+
+// on:
+//   push:
+//     branches:
+//       - main
+//       - "v*.*.*"
+
+// jobs:
+//   build:
+//     runs-on: ubuntu-latest
+//     permissions:
+//       contents: read
+//       packages: write
+//     steps:
+//       - uses: actions/checkout@v4
+//       - uses: actions/setup-node@v3
+//         with:
+//           node-version: '20.x'
+//           registry-url: 'https://npm.pkg.github.com'
+//           scope: '@msaadart'
+//         env:
+//           NODE_AUTH_TOKEN: ${{ secrets.TOKEN_ACCESS }}
+//       # - run: |
+//       #     curl -H "Authorization: Bearer $NODE_AUTH_TOKEN" \
+//       #     -H "Accept: application/vnd.github.v3+json" \
+//       #     https://api.github.com/repos/${{ github.repository }}
+//       - run: npm install
+//       - run: npm run build shared
+//       - run: |
+//           cd dist/shared
+//           npm publish --access public
