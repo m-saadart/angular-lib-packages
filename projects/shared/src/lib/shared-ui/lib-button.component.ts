e750558ec715bf5bca17e6a52619697e587ea863
saad-ihhn <saad.ihhn@gmail.com>
2025-02-08 19:26:13 +0500

    add button component

---
+++ projects/shared/src/lib/shared-ui/lib-button.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib-button',
+  imports: [],
+  template: `
+    <p>
+      lib-button works!
+    </p>
+  `,
+  styles: ``
+})
+export class LibButtonComponent {
+
+}
